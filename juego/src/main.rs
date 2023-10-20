3579081a53ccb7851c7c31e7fe495b9b62d13a86
Petroshka00 <petrocinitomas@gmail.com>
2023-10-20 14:57:12 -0300

    Primeras estructuras y poco mas

---
+++ juego/src/main.rs
@@ -0,0 +1,54 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+struct Juego{
+    mapa: [[i32; 10]; 10], // A decidir el tamaño
+}
+
+struct Posicion{
+    pos_x: u8,    
+    pos_y: u8,
+}
+
+struct Arma{
+    daño: u8,
+    punteria: u8,
+}
+
+struct Armadura{
+    armadura: u8,
+    esquiva: u8,
+}
+
+struct Entidad{
+    posicion: Posicion,
+    salud: u8,
+    daño: u8,
+    armadura: u8,
+    punteria: u8,
+    esquiva: u8,
+}
+
+fn combate(mut ent_1: Entidad, mut ent_2: Entidad){
+    let chance_de_golpe: u8 = ent_1.punteria - ent_2.esquiva;
+    let chance_minima_necesaria: u8 = rand::thread_rng().gen_range(1..=100);
+
+    match chance_de_golpe.cmp(&chance_minima_necesaria){
+        Ordering::Less => println!("Le erro"),// No le pega directamente
+        Ordering::Equal => println!("Cosas"), //Le dio justo, capaz un golpe critico?
+        Ordering::Greater => ent_2.salud = ent_2.salud - ent_1.daño, // Le hace el daño que deberia, aca agregar algo con la armadura
+    }
+
+
+
+}
+
+fn inicializar_mapa(){
+
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
