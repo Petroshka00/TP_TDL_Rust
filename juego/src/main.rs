73af56baf5ea03fcedcdb870396a9113eac3f948
Petroshka00 <petrocinitomas@gmail.com>
2023-11-28 12:37:13 -0300

    test de variedad de monstruo

--- juego/src/main.rs
+++ juego/src/main.rs
@@ -182,21 +182,28 @@ fn main() -> rltk::BError {
         .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
         .build();
 
+    let mut rng = rltk::RandomNumberGenerator::new();
     for room in map.rooms.iter().skip(1) {
         let (x,y) = room.center();
+
+        let glyph : rltk::FontCharType;
+        let roll = rng.roll_dice(1, 2);
+        match roll {
+            1 => { glyph = rltk::to_cp437('g') }
+            _ => { glyph = rltk::to_cp437('o') }
+        }
+
         gs.ecs.create_entity()
         .with(Position{ x, y })
         .with(Renderable{
-            glyph: rltk::to_cp437('g'),
+            glyph: glyph,
             fg: RGB::named(rltk::RED),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Viewshed{ visible_tiles : Vec::new(), range: 8, dirty: true })
         .build();
-}
-
-gs.ecs.insert(map);
-
+    }
+    gs.ecs.insert(map);
     rltk::main_loop(context, gs)
 }
 */
