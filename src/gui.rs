a858457a233b7d306fe6782c86029566b383bf77
Petroshka00 <petrocinitomas@gmail.com>
2023-11-30 16:40:08 -0300

    Contador de vida y barra de vida (sujeta a cambios)

--- src/gui.rs
+++ src/gui.rs
@@ -1,6 +1,7 @@
 use rltk::{ RGB, Rltk, Console };
 use specs::prelude::*;
 
+use super::{CombatStats, Player};
 use crate::{SCREEN_HEIGHT, SCREEN_WIDTH};
 
 const GUI_WIDTH : usize = SCREEN_WIDTH;
@@ -9,4 +10,13 @@ const GUI_HEIGHT : usize = 6;
 
 pub fn draw_ui(ecs: &World, ctx : &mut Rltk) {
     ctx.draw_box(0, SCREEN_HEIGHT - GUI_HEIGHT - 1, GUI_WIDTH - 1, GUI_HEIGHT, RGB::named(rltk::WHITE), RGB::named(rltk::BLACK));
+
+    let combat_stats = ecs.read_storage::<CombatStats>();
+    let players = ecs.read_storage::<Player>();
+    for (_player, stats) in (&players, &combat_stats).join() {
+        let health = format!(" HP: {} / {} ", stats.hp, stats.max_hp);
+        ctx.print_color(12, SCREEN_HEIGHT - GUI_HEIGHT - 1, RGB::named(rltk::YELLOW), RGB::named(rltk::BLACK), &health);
+
+        ctx.draw_bar_horizontal(28, SCREEN_HEIGHT - GUI_HEIGHT - 1, 51, stats.hp, stats.max_hp, RGB::named(rltk::RED), RGB::named(rltk::BLACK));
+    }
 }
